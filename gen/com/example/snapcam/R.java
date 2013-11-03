1c565960f0d5def391c5e69930aa40e71e7e93d6
yuqiaoyan <yuqiaoyan@gmail.com>
2013-11-02 20:37:00 -0700

    adding resources

--- gen/com/example/snapcam/R.java
+++ gen/com/example/snapcam/R.java
@@ -22,9 +22,16 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int mic_on=0x7f020001;
+        public static final int shutter=0x7f020002;
+        public static final int shutter_touchdown=0x7f020003;
+        public static final int snapcam_launcher=0x7f020004;
+        public static final int switch_touchdown=0x7f020005;
+        public static final int switch_touchup=0x7f020006;
     }
     public static final class id {
         public static final int camera_preview=0x7f080000;
+        public static final int shutter=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
